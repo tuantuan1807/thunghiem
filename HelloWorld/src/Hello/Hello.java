9f126cccac26378bd4a7017833e1d1ffb9b85dcd
HoangBao29 <133729617+HoangBao29@users.noreply.github.com>
2024-09-14 16:01:43 +0700

    Update Hello.java

--- HelloWorld/src/Hello/Hello.java
+++ HelloWorld/src/Hello/Hello.java
@@ -3,7 +3,7 @@ package Hello;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("He xo lo he xo ly ly ");
+		System.out.println("Bảo đẹp giai top 1 từ dưới lên - đó là lời nói dối ");
 	}
 
 }
